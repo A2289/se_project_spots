da5c585ea688cf657e8e58beb4380ca0b7beef29
A2289 <perezalberto2289@gmail.com>
2025-07-29 16:15:50 -0500

    YOUR COMMIT MESSAGE

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,46 @@
+const initialCards = [
+  {
+  name: "Val Thorens",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+},
+];
+
+const initialCards = [
+  {
+  name: "Resturant terrace",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+},
+];
+
+const initialCards = [
+  {
+  name: "An outdoor cafe",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+},
+];
+
+const initialCards = [
+  {
+  name: "A very long bridge, over the forest and through the trees",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+},
+];
+
+const initialCards = [
+  {
+  name: "Tunnel with morning light",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg
+",
+},
+];
+
+const initialCards = [
+  {
+  name: "Mountain house",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+},
+];
+
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
@@ -56,3 +99,7 @@ function handlenewPostSubmit(evt) {
 }
 
 newPostForm.addEventListener("submit", handlenewPostSubmit);
+
+initialCards.forEach(function (item) {
+console.log(item.name, item.link); // Print initial cards to console 
+});
